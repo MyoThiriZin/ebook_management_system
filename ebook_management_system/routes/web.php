5ad704fecce76a3ab8891955f9b9ad9258c4fd72
wintshwesin1 <wintshwesin211@gmail.com>
2022-03-21 21:29:28 +0630

    [wintshwesin] fix bugs

--- ebook_management_system/routes/web.php
+++ ebook_management_system/routes/web.php
@@ -28,6 +28,15 @@ Route::group(['middleware' => ['admin']], function () {
 
     //Dashboard
     Route::get('/dashboard', 'Admin\DashboardController@index')->name('dashboard.index');
+
+    //Category
+    Route::get('/categories', [CategoryController::class, 'index'])->name('categories');
+    Route::get('/search', [CategoryController::class, 'search'])->name('category.search');
+    Route::get('/category/create', [CategoryController::class, 'create'])->name('category.create');
+    Route::get('/category/edit/{category}', [CategoryController::class, 'edit'])->name('category.edit');
+    Route::post('/category/update/{id}', [CategoryController::class, 'update'])->name('category.update');
+    Route::post('/category/delete/{id}', [CategoryController::class, 'destroy'])->name('category.destroy');
+    Route::post('/category/create', [CategoryController::class, 'store'])->name('category.store');
 });
 
 Route::get('/register', 'Admin\Auth\AuthController@showRegistrationView')->name('register');
@@ -42,9 +51,3 @@ Route::post('forget-password', 'Admin\Auth\ForgotPasswordController@submitForget
 Route::get('reset-password/{token}', 'Admin\Auth\ForgotPasswordController@showResetPasswordForm')->name('reset.password');
 Route::post('reset-password', 'Admin\Auth\ForgotPasswordController@submitResetPasswordForm')->name('submit.reset.password');
 
-//Authors
-Route::resource('authors','Admin\Ajax\AuthorController');
-Route::get('/search', 'Admin\Ajax\AuthorController@search');
-
-//Dashboard
-Route::get('/dashboard', 'Admin\DashboardController@index')->name('dashboard.index');
