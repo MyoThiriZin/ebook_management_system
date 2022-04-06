273fce1af9b5ed04d49e41fcce3c911301d72613
Phyo Phyo Aung <pepeag.1995@gmail.com>
2022-04-06 15:25:07 +0630

    [pepeag] fix admin profile design

--- ebook_management_system/resources/views/adminprofile.blade.php
+++ ebook_management_system/resources/views/adminprofile.blade.php
@@ -4,16 +4,16 @@
   <div class="seemore-container">
     <h2 class="seemore-ttl">Admin Profile</h2>
     <div class="seemore-item">
-      <label for="" class="">Name :</label><span>{{ $users->name }}</span>
+      <label for="" class="profile-name">Name &nbsp;:</label><span class="profile-content">{{ $users->name }}</span>
     </div>
     <div class="seemore-item">
-      <label for="">Email :</label><span>{{ $users->email }}</span>
+      <label for="" class="profile-name">Email &nbsp;:</label><span class="profile-content">{{ $users->email }}</span>
     </div>
     <div class="seemore-item">
-      <label for="">Phone :</label><span>{{ $users->phone_no }}</span>
+      <label for="" class="profile-name">Phone &nbsp;:</label><span class="profile-content">{{ $users->phone_no }}</span>
     </div>
     <div class="seemore-item">
-      <label for="">Address :</label><span>{{ $users->address }}</span>
+      <label for="" class="profile-name">Address &nbsp;:</label><span class="profile-content">{{ $users->address }}</span>
     </div>
   </div>
 </div>
