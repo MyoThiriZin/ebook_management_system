7d579746ba319bc40da61cc243c30b5d1025dd0d
hansulinnhtet <hansulinnhtet@uit.edu.mm>
2022-04-06 17:59:36 +0630

    [HSLH] fixs

--- ebook_management_system/app/Contracts/Services/DashboardServiceInterface.php
+++ ebook_management_system/app/Contracts/Services/DashboardServiceInterface.php
@@ -2,13 +2,32 @@
 
 namespace App\Contracts\Services;
 
+/**
+ * Interface for dashboard service.
+ */
 interface DashboardServiceInterface
 {
+  /**
+   * To get chart of user counts
+   * @return array list of book labels and user counts
+   */
   public function getchart();
 
+  /**
+   * To get book list
+   * @return array list of books
+   */
   public function getbooks();
 
+  /**
+   * To get author list
+   * @return array list of authors
+   */
   public function getauthors();
 
+  /**
+   * To get category list
+   * @return array list of categories
+   */
   public function getcategories();
 }
